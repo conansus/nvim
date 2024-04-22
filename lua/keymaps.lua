b50280563fdc9f700dde50ef877c9eaff8ecd3af
conansus <akhram050500@gmail.com>
2024-04-22 12:04:38 +0800

    add plugins to my nvim configs

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,13 @@
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+-- improve indentation
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- improve movement line up and down
+vim.keymap.set("v", "<S-j>", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "<S-k>", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
+
+--
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
